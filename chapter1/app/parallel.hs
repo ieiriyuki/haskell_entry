42594401870a67c4d044df4924979c9c95befd41
ieiriyuki <ieiriyuki@example.com>
2023-10-14 19:48:45 +0900

    refactor: parallelize

--- chapter1/app/parallel.hs
+++ chapter1/app/parallel.hs
@@ -1,6 +1,6 @@
 import Prelude
 import Data.Int
-import Control.Parallel.Strategies
+import Control.Parallel
 
 isPrime :: Int32 -> Bool
 isPrime x = all (\n -> x `mod` n /= 0) [2 .. toEnum (floor $ sqrt $ fromIntegral x)]
@@ -10,5 +10,13 @@ arr = [ 2 .. 1000000 ]
 
 main :: IO ()
 main = do
-    let arr' = map isPrime arr `using` parListChunk 256 rpar
-    putStr "primes: " >> print (length $ filter id arr')
+    let primes = reduceP (fromEnum . isPrime) (+) arr -- 判定しながら集計
+    putStr "primes: " >> print primes
+
+reduceP :: (b -> a) -> (a -> a -> a) -> [b] -> a
+reduceP f _ [x] = f x
+reduceP f (<+>) xs = (ys `par` zs) `pseq` (ys <+> zs) where
+    len = length xs
+    (ys', zs') = splitAt (len `div` 2) xs
+    ys = reduceP f (<+>) ys'
+    zs = reduceP f (<+>) zs'
