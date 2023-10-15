762e39ed094ac6204d292dc5203fb0db699f5a06
ieiriyuki <ieiriyuki@example.com>
2023-10-15 13:18:22 +0900

    refactor: object of escaped string

--- chapter1/app/escape.hs
+++ chapter1/app/escape.hs
@@ -1,16 +1,9 @@
 -- echo '"&<>"' | stack runghc escape.hs
 
-escape :: String -> String
-escape str = str >>= escapeAmp >>= escapeOther where
-    escapeAmp   '&' = "&amp;"
-    escapeAmp     c = [c]
-    escapeOther '<' = "&lt;"
-    escapeOther '>' = "&gt;"
-    escapeOther '"' = "&quot;"
-    escapeOther   c = [c]
+import HTMLEscapedString
 
 main :: IO()
 main = do
     rawString <- getLine
     let escapedString = escape rawString
-    putStrLn escapedString
+    putHTMLEscapedStrLn escapedString
