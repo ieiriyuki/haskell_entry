efadd249250f3406025f2b9558c5b9a9a4083e08
ieiriyuki <ieiriyuki@example.com>
2023-12-07 23:26:23 +0900

    feat: sample type class code

--- chapter5/src/Lib.hs
+++ chapter5/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      BoolLike,
+      fromBoolLike,
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+class BoolLike a where
+    fromBoolLike :: a -> Bool
+
+instance BoolLike Int where
+    fromBoolLike = (0 /=)
+
+instance BoolLike (Maybe a) where
+    fromBoolLike Nothing = False
+    fromBoolLike (Just _) = True
+
+instance BoolLike Bool where
+    fromBoolLike x = x
