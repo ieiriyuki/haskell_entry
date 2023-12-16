05d8dfeff0ae3462ee8b84392e64a094cc2ce17f
ieiriyuki <ieiriyuki@example.com>
2023-12-16 13:13:08 +0900

    feat: sample reader code

---
+++ chapter5/src/Reader.hs
@@ -0,0 +1,23 @@
+-- docker compose up -d
+-- docker compose exec app bash
+-- ghci src/Reader.hs
+import Control.Monad.Reader
+
+data Config = Config { verbose :: Int
+                     , debug :: Bool
+                     }
+
+configToLevel :: Config -> Int
+configToLevel config
+    | debug config = 10
+    | otherwise    = verbose config
+
+outputLevel :: Reader Config [Int]
+outputLevel = do
+    config <- ask
+    return [ 1 .. configToLevel config ]
+
+output :: Int -> String -> Reader Config (Maybe String)
+output level str = do
+    ls <- outputLevel
+    return (if level `elem` ls then Just str else Nothing)
