f9162a24f688db842e869b5e7f5d0e464c818fde
ieiriyuki <ieiriyuki@example.com>
2024-02-05 21:10:57 +0900

    refactor: inline toPair

--- chapter6/src/RLE.hs
+++ chapter6/src/RLE.hs
@@ -11,10 +11,7 @@ import Data.List (group)
 -- >>> rle "AAABBCCCCAAA"
 -- "A3B2C4A3"
 rle :: String -> String
-rle = concatMap (\s -> rl2str (toPair s)) . group
+rle = concatMap (\s -> rl2str (head s, length s)) . group
 
 rl2str :: (Char, Int) -> String
 rl2str (c, n) = c : show n
-
-toPair :: String -> (Char, Int)
-toPair str = (head str, length str)
