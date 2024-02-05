159431f49f7b6fa901aa3c614dae2accab6cd90b
ieiriyuki <ieiriyuki@example.com>
2024-02-05 20:21:05 +0900

    feat: sample RLE code

---
+++ chapter6/src/RLE.hs
@@ -0,0 +1,14 @@
+module RLE where
+
+import Data.List (group)
+
+-- | ランレングス圧縮
+--
+-- >>> rle ""
+-- ""
+-- >>> rle "A"
+-- "A1"
+-- >>> rle "AAABBCCCCAAA"
+-- "A3B2C4A3"
+rle :: String -> String
+rle = concatMap (\s -> head s : show (length s)) . group
