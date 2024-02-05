c8a9a21db19bab5b7b6f14ea9cfb1eee8f151e2a
ieiriyuki <ieiriyuki@example.com>
2024-02-05 20:35:04 +0900

    chore: another intermediate state of code

--- chapter6/src/RLE.hs
+++ chapter6/src/RLE.hs
@@ -17,7 +17,13 @@ fromCharAndRunLength :: [(Char, Int)] -> String
 fromCharAndRunLength = concat . rls2strs
 
 rls2strs :: [(Char, Int)] -> [String]
-rls2strs = undefined
+rls2strs = foreach rl2str
+
+foreach :: (a -> b) -> [a] -> [b]
+foreach = undefined
+
+rl2str :: (Char, Int) -> String
+rl2str = undefined
 
 toCharAndRunLength :: String -> [(Char, Int)]
 toCharAndRunLength = undefined
