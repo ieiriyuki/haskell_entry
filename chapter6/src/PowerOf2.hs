fc8d25c15b77ceb370c2e8de5b46300f0623738c
ieiriyuki <ieiriyuki@example.com>
2024-02-06 18:50:58 +0900

    feat: power of 2

---
+++ chapter6/src/PowerOf2.hs
@@ -0,0 +1,22 @@
+module PowerOf2 where
+
+-- | 2の累乗に対し, それが2の何乗かを返す
+--   それ以外の時はエラーになる
+--
+-- >>> exponentPowerOf2 1
+-- 0
+-- >>> exponentPowerOf2 2
+-- 1
+-- >>> exponentPowerOf2 3
+-- *** Exception: 3 is not a power of 2
+-- >>> exponentPowerOf2 4
+-- 2
+-- >>> exponentPowerOf2 1024
+-- 10
+exponentPowerOf2 :: Int -> Int
+exponentPowerOf2 = expof2 0 where
+    expof2 :: Int -> Int -> Int
+    expof2 r n
+        | n == 1         = r
+        | n < 1 || odd n = error (shows n " is not a power of 2")
+        | otherwise      = expof2 (r + 1) (n `div` 2)
